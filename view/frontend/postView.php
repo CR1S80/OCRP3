535a4535658bab08e3451361d43bc0b3efe555d6
CR1S80 <47815383+CR1S80@users.noreply.github.com>
2019-04-19 19:28:22 +0200

    css du postView

--- view/frontend/postView.php
+++ view/frontend/postView.php
@@ -8,46 +8,52 @@
 </head>
 
 <div class="content">
-<a class="btn btn-primary back row" href="http://localhost/CoursPHP/TPBlog/OCRP3/" data-original-title="" title=""><i class="far fa-arrow-alt-circle-left"></i> Retour à la liste des chapitres</a>
-<div class="header-title">
-    <h1><?= $title; ?></h1>
-    <em>le <?= $post->getCreation_date(); ?></em>
-</div>
-<div class="article">
-    
-       
-        
-       
-    
-    
-    <div class="article-content">
-        <?= $post->getContent(); ?>
+    <a class="btn btn-primary back row" href="http://localhost/CoursPHP/TPBlog/OCRP3/" data-original-title="" title=""><i class="far fa-arrow-alt-circle-left"></i> Retour à la liste des chapitres</a>
+    <div class="header-title">
+        <h1><?= $title; ?></h1>
+        <em>le <?= $post->getCreation_date(); ?></em>
     </div>
-</div>
+    <div class="article">
 
-<h2 id="comment-ancre">Commentaires</h2>
 
-<form action="index.php?action=addComment&amp;id=<?= $post->getId(); ?>" method="post">
-    <div>
-        <label for="author">Auteur</label><br />
-        <input type="text" id="author" name="author" />
+
+
+
+
+        <div class="article-content">
+            <?= $post->getContent(); ?>
+        </div>
     </div>
-    <div>
-        <label for="comment">Commentaire</label><br />
-        <input type="text" id="comment" name="comment">
+    <div class="header-title">
+        <h1 id="comment-ancre">Commentaires</h1>
     </div>
-    <div>
-        <input type="submit" />
-    </div>
-</form>
 
-<?php 
- 
-foreach($comments as $comment): ?>
+    <form action="index.php?action=addComment&amp;id=<?= $post->getId(); ?>" method="post">
+        <div>
+            <label for="author">Auteur</label><br />
+            <input type="text" id="author" name="author" />
+        </div>
+        <div>
+            <label for="comment">Commentaire</label><br />
+            <input type="text" id="comment" name="comment">
+        </div>
+        <div>
+            <input type="submit" />
+        </div>
+    </form>
 
-    <p><strong><?= $comment->getAuthor(); ?></strong> le <?= $comment->getComment_date(); ?></p>
-    <p><?= $comment->getComment(); ?></p> 
-    <a href="http://localhost/CoursPHP/TPBlog/OCRP3/?action=reportComment&id=<?= $comment->getId(); ?>">Signaler</a>
+    <?php foreach ($comments as $comment): ?>
+        <div class="comment-space">
+            <div class="comment-author"><?= $comment->getAuthor(); ?> a commenté :</div>
+            <hr>
+            <p class="comment-text"><?= $comment->getComment(); ?> </p>
+            <div class="bottom-comment">
+                <div class="comment-date"><?= $comment->getComment_date(); ?></div>
+                <div class="comment-action"><a href="http://localhost/CoursPHP/TPBlog/OCRP3/?action=reportComment&id=<?= $comment->getId(); ?>">Signaler</a>
+                </div>
+            </div>
+
+        </div>
 
 <?php endforeach; ?>
 </div>
